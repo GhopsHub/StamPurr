c41212b6fabf20b1fb32e3bf783380005d95e918
GhostGoju <129934609+GhostGoju@users.noreply.github.com>
2025-07-14 09:52:35 -0500

    Configuracioan inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
+    <BrowserRouter basename="/Cuart">
+      <div className="page-wrapper">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </BrowserRouter>
   )
 }
 
